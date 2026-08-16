35b771ce4ffe5a1e7c316ec3dc102ee520649f81
Karrar-Hussain <karrarh440@gmail.com>
2026-08-16 16:37:51 +0500

    Add Next Round [A] from Codeforces

---
+++ 158/A - Next Round/solution.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // Write C++ code here
+    int n,k;
+    cin>>n;
+    cin>>k;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    int count=0;
+      for(int i=0; i<n; i++){
+       if(arr[i]>=arr[k-1]&&arr[i]!=0){
+           count++;
+       }
+      }
+      cout<<count;
+}
