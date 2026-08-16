03c13e1e39a8a2ef049d807375eb37d16c714f48
Karrar-Hussain <karrarh440@gmail.com>
2026-08-16 16:41:45 +0500

    Add Trippi Troppi [A] from Codeforces

---
+++ 2094/A - Trippi Troppi/solution.cpp
@@ -0,0 +1,18 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+ 
+ 
+int main() {
+    // Write C++ code here
+   int test;
+   cin>>test;
+   
+   while(test--){
+       int i=0;
+      string str1,str2,str3;
+      cin>>str1>>str2>>str3;
+      cout<<str1[i]<<str2[i]<<str3[i]<<endl;
+   }
+    return 0;
+}
