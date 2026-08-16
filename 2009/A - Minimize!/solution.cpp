a9400c9e5231f71164866c5640f3bc3eee096621
Karrar-Hussain <karrarh440@gmail.com>
2026-08-16 16:39:51 +0500

    Add Minimize! [A] from Codeforces

---
+++ 2009/A - Minimize!/solution.cpp
@@ -0,0 +1,14 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    // Write C++ code here
+  int test;
+  cin>>test;
+  int a,b;
+  while(test--){
+      cin>>a>>b;
+      cout<<b-a<<endl;
+  }
+    return 0;
+}
