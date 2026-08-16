579688843c5c09a8aae6e5ee1fe09814571c6232
Karrar-Hussain <karrarh440@gmail.com>
2026-08-16 16:37:34 +0500

    Add Destroying Towers [A] from Codeforces

---
+++ 2237/A - Destroying Towers/solution.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+int main(){
+    int t;
+    cin >> t;
+    while (t--){
+        int n;
+        cin>>n;
+        long long ans = 0;
+        int mn =INT_MAX;
+        for (int i=0; i<n; i++){
+     int x;
+     cin>>x;
+  mn=min(mn,x);
+     ans+=mn;
+        }
+        cout<<ans<<endl;
+    }
+    return 0;
+}
