e6dd4a56782c11c96eb34613bddcf196bdc6c5de
Karrar-Hussain <karrarh440@gmail.com>
2026-08-16 16:36:40 +0500

    Add Easy Problem [A] from Codeforces

---
+++ 2044/A - Easy Problem/solution.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // Write C++ code here
+    int test ;
+    cin  >> test ;
+    int a  , n ;
+    for ( int i=0 ; i< test; i++){ 
+        cin  >> a ;
+        n = a - 1 ;
+    cout  << n << endl ;
+}
+    return 0;
+}
