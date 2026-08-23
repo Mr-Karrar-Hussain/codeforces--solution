79ec81c70508c96304922145a96a6413c0374a61
Karrar-Hussain <karrarh440@gmail.com>
2026-08-23 21:13:32 +0500

    Add Ultra-Fast Mathematician [A] from Codeforces

---
+++ 61/A - Ultra-Fast Mathematician/solution.cpp
@@ -0,0 +1,17 @@
+// Online C++ compiler to run C++ program online
+#include<iostream>
+using namespace std;
+ 
+ 
+int main(){
+string n,m;
+cin>>n>>m;
+for(int i=0; i<n.length(); i++){
+    if(n[i]==m[i]){
+        cout<<0;
+    }else{
+        cout<<1;
+    }
+}
+}
+ 
