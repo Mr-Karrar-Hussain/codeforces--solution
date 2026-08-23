ac9dd325a19be501de39d30cd5ba2710e2680071
Karrar-Hussain <karrarh440@gmail.com>
2026-08-23 21:13:21 +0500

    Add Football [A] from Codeforces

---
+++ 96/A - Football/solution.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+ 
+ 
+ 
+int main (){
+string str;
+cin>>str;
+int count =1;
+for(int i=1; i<str.length();  i++){
+if(str[i]==str[i-1]){
+  count++;
+}
+  else{
+    count=1;
+  }
+  if(count>=7){
+  cout<<"YES"<<endl;
+  return 0;
+}
+}
+ 
+  cout<<"NO"<<endl;
+}
